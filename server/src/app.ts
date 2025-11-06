d3cccd0b57a2d757686dadda84d3ff077da0518d
SayanSutradhar1 <sayan321x@gmail.com>
2025-11-07 01:24:22 +0530

    minor

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,70 +1,55 @@
-import { Express, Router } from "express";
-import express from "express";
-import { IncomingMessage, Server, ServerResponse } from "http";
-import cors from "cors";
+import Application from "./server";
+import { dbconnect } from "./config/db.config";
+import { errorMiddleware } from "./middlewares/error.middleware";
+import { blogRouter } from "./routes/blog.route";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import { userRouter } from "./routes/user.route";
+import express from "express"
 
-/**
- * Application class for setting up and managing an Express server instance
- */
-class Application {
-  /** Port number for the server to listen on */
-  port!: number;
-  /** Name of the application */
-  appName!: string;
-  /** Express application instance */
-  instance!: Express;
-  /** HTTP Server instance */
-  server!:Server<typeof IncomingMessage,typeof ServerResponse>
+dotenv.config({
+  path: "./.env",
+});
 
-  /**
-   * Creates an instance of Application
-   * @param {number} port - The port number to run the server on
-   * @param {string} appName - The name of the application
-   */
-  constructor(port: number, appName: string) {
-    this.port = port;
-    this.appName = appName;
-  }
+const app = new Application(Number(process.env.PORT) || 3000, "Blog App");
 
-  /**
-   * Initializes the Express application with JSON parsing and CORS configuration
-   */
-  init(){
-    this.instance = express();
-    this.instance.use(express.json())
-    this.instance.use(cors({
-        origin:process.env.CLIENT_URL,
-        credentials:true,
-        methods:["GET","POST","PUT","DELETE"],
-    }))
-  }
+app.init();
 
-  /**
-   * Adds one or more Express routers to the application
-   * @param {...Router[]} routers - Express Router instances to add
-   */
-  useRouter(...routers:{path:string,router:Router}[]){
-    for (const router of routers) {
-        this.instance.use('/api'+router.path,router.router)
-    }
-  }
+app.instance.use(express.json())
+app.instance.use(express.urlencoded({ extended: true }))
 
 
-  /**
-   * Starts the server on the configured port
-   * @param {Function} cb - Callback function to execute when server starts
-   */
-  start(cb: Function) {
-    this.server = this.instance.listen(this.port, cb());
-  }
-
-  /**
-   * Stops the server
-   * @param {Function} cb - Callback function to execute when server stops
-   */
-  stop(cb:Function) {
-    this.server.close(cb());
-  } 
+if(process.env.NODE_ENV === "development"){
+  app.instance.use(
+    morgan(
+      'dev'
+    )
+  )
 }
 
-export default Application;
+
+app.useRouter(
+  {
+    path: "/blog",
+    router: blogRouter,
+  },
+  {
+    path: "/user",
+    router: userRouter,
+  }
+);
+
+
+
+app.instance.use(errorMiddleware);
+
+dbconnect()
+  .then(() =>
+    app.start(() => {
+      console.log(`${app.appName} is running on port ${app.port}`);
+    })
+  )
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
